2fe9dfc0ebae266f7a8ddcb13feee3b0cce4673a
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2024-07-31 18:57:52 -0300

    gameover menu panel

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -4,19 +4,23 @@ extends Node2D
 @onready var hud = $HUD
 @onready var gameover_timer = $GameOverTimer
 @onready var refuel = $HUD/Refuel
+
 var engine_on:bool = false
 var launched:bool = false
-
+var rocket_hight = 0
 func _ready():
 	gameover_timer.start()
 
 func _process(_delta):
-	if engine_on:
-		camera.apply_shake()
 	var rocket_velocity = round(rocket.linear_velocity.y)
 	var time_left = gameover_timer.time_left
 	var rocket_fuel = rocket.fuel
-	hud.update_stats(rocket_velocity, time_left, rocket_fuel)
+	
+	if engine_on:
+		camera.apply_shake()
+		rocket_hight = -round(rocket.global_position.y)
+		
+	hud.update_stats(rocket_velocity, time_left, rocket_fuel, rocket_hight)
 	
 	if Input.is_action_just_pressed("lauch"):
 		if not launched:
@@ -25,14 +29,13 @@ func _process(_delta):
 
 func _on_refuel_refuel(value:int):
 	rocket.fuel += value
-
+	rocket.fuel = clamp(rocket.fuel, 0, 100)
 
 func launch_rocket():
 	launched = true
 	engine_on = true
 	refuel.hide()
 	rocket.start_engine()
-	print('LAUNCH')
 
 
 func _on_rocket_engine_off():
@@ -42,4 +45,6 @@ func _on_rocket_engine_off():
 func _on_timer_timeout():
 	rocket.explode()
 	var tween = create_tween()
-	tween.tween_property(camera, "zoom", Vector2(0.3,0.3), 2)
+	tween.tween_property(camera, "zoom", Vector2(0.3,0.3), 1)
+	print('rocket_hight ', rocket_hight)
+	hud.gameover_menu(rocket_hight, false)
--- Scenes/Hud/hud.gd
+++ Scenes/Hud/hud.gd
@@ -4,8 +4,42 @@ extends CanvasLayer
 @onready var time_left_label = $TimeLeft/TimeLeftLabel
 @onready var fuel_bar = $FuelBar
 
+@onready var hight_label = $Hight/HightLabel
+@onready var body_label = $GameoverPanel/VBoxContainer/BodyLabel
+@onready var game_over_label = $GameoverPanel/VBoxContainer/GameOver
+@onready var reached_hight_label = $GameoverPanel/VBoxContainer/ReachedHight
+@onready var gameover_panel = $GameoverPanel
 
-func update_stats(velocity, time, fuel):
+
+func update_stats(velocity, time, fuel, hight):
 	velocity_label.text = str(-velocity)
 	time_left_label.text = str(time).pad_decimals(2)
 	fuel_bar.value = fuel
+	hight_label.text = str(hight)
+
+
+func gameover_menu(hight:int, ground:bool):
+	gameover_panel.show()
+	if ground:
+		game_over_label.text = 'GAME OVER'
+		body_label.text = 'you hit the ground!'
+		reached_hight_label.text += '0 meters'
+	else:
+		if hight < 100:
+			body_label.text = 'danger explosion hight'
+		elif hight < 1000:
+			body_label.text = 'bad explosion hight'
+		elif hight < 5000:
+			body_label.text = 'good explosion hight'
+		elif hight < 10000:
+			body_label.text = 'great explosion hight'
+		else:
+			body_label.text = 'amazing explosion hight'
+
+
+func _on_try_again_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game/game.tscn")
+
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
